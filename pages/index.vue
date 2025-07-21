c06508e4eff79915ec9c0139cfbf4118911b5d7b
mollani0704 <mollani0704@gmail.com>
2025-07-21 22:59:32 +0900

    [init] nuxt.js 프로젝트 세팅

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>🏠 홈닉 클론코딩에 오신 걸 환영합니다!</h1>
+    <p>Nuxt 3 + Vue 3 + TypeScript로 쇼핑몰 클론코딩 시작!</p>
+    <NuxtLink to="/products" class="link">👉 상품 보러가기</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.home {
+  padding: 2rem;
+  text-align: center;
+}
+.link {
+  display: inline-block;
+  margin-top: 1rem;
+  color: #0070f3;
+  font-weight: bold;
+  text-decoration: underline;
+}
+</style>
